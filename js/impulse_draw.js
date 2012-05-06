a631a7f90edb7a881882cb7c0f03ec6141c68754
Mark Zhang <mazhang@mit.edu>
2012-05-06 03:18:42 -0400

    controllable player

--- js/impulse_draw.js
+++ js/impulse_draw.js
@@ -1,56 +1,28 @@
-require = window.require;
+var draw_factor = 10; //everything is scaled by this factor
 
-require.define({
-    'impulse_draw': function(require, exports, module) {
-
-var draw = require('gamejs/draw');
-console.log(EXPORTS)
-    console.log(exports)
-exports.drawWorld = function(world, surface) {
+function drawWorld(world, context) {
 	for (var b = world.m_bodyList; b; b = b.m_next) {
 		for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
-			drawShape(s, surface);
+			drawShape(s, context);
 		}
 	}
 };
 
-var drawShape = exports.drawShape = function(shape, surface) {
+function drawShape(shape, context) {
+    context.beginPath()
+	context.fillStyle = 'black';
 	switch (shape.m_type) {
 	case b2Shape.e_circleShape:
 		{
 			var circle = shape;
 			var pos = circle.m_position;
 			var r = circle.m_radius;
-			var segments = 16.0;
-			var theta = 0.0;
-			var dtheta = 2.0 * Math.PI / segments;
-            draw.circle(surface, '#000000', [pos.x, pos.y], circle.m_radius, 0)
 			// draw circle
-			/*context.moveTo(pos.x + r, pos.y);
-			for (var i = 0; i < segments; i++) {
-				var d = new b2Vec2(r * Math.cos(theta), r * Math.sin(theta));
-				var v = b2Math.AddVV(pos, d);
-				context.lineTo(v.x, v.y);
-				theta += dtheta;
-			}
-			context.lineTo(pos.x + r, pos.y);*/
+			context.arc(pos.x*draw_factor, pos.y*draw_factor, r*draw_factor, 0, 2*Math.PI, true)
+		}
+		break;
 	
-		}
-		break;
-	case b2Shape.e_polyShape:
-		{
-			var poly = shape;
-			var tV = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
-            //TODO
-			//context.moveTo(tV.x, tV.y);
-			for (var i = 0; i < poly.m_vertexCount; i++) {
-				var v = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
-				//context.lineTo(v.x, v.y);
-			}
-			//context.lineTo(tV.x, tV.y);
-		}
-		break;
 	}
-	//context.stroke();
+	context.fill();
 }
-}}, ['gamejs/draw']);
+
--- js/impulse_main.js
+++ js/impulse_main.js
@@ -1,62 +1,124 @@
-/**
- * A bare bones Sprite and sprite Group example.
- *
- * We move a lot of Ship sprites across the screen with varying speed. The sprites
- * rotate themselves randomly. The sprites bounce back from the bottom of the
- * screen.
- */
-
-var gamejs = require('gamejs');
-var impulse_draw = require('impulse_draw');
-console.log(impulse_draw);
-console.log(gamejs)
-
+var ctx;
+var world;
+var canvasWidth, canvasHeight;
+var player;
+var player_x = 0;
+var player_y = 0;
+var player_force = .5;
+var player_lin_damp = .96;
 
 function setupWorld() {
     var worldAABB = new b2AABB();
     worldAABB.minVertex.Set(-1000, -1000);
     worldAABB.maxVertex.Set(1000, 1000);
     var gravity = new b2Vec2(000, 000);
-    var doSleep = true;
+    var doSleep = false; //objects in our world will rarely go to sleep
     world = new b2World(worldAABB, gravity, doSleep); 
     makePlayer()
-    
 }
 
 function makePlayer()
 {
     var circleSd = new b2CircleDef();
     circleSd.density = 1.0;
-    circleSd.radius = 20;
+    circleSd.radius = .5;
     circleSd.restitution = 1.0;
     circleSd.friction = 0;
     var circleBd = new b2BodyDef();
     circleBd.AddShape(circleSd);
-    circleBd.position.Set(200,200);
-    var circleBody = world.CreateBody(circleBd);
+    circleBd.position.Set(20,20);
+    player = world.CreateBody(circleBd);
+    player.m_linearDamping = player_lin_damp;
 }
 
-function main() {
+function step() {
+    processGame();
+    world.Step(1.0/60, 1);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawWorld(world, ctx);
+
+	setTimeout('step()', 10);
+}
+
+
+
+Event.observe(window, 'load', function() {
+    canvasWidth = 800;
+    canvasHeight = 600;
    // screen setup
-   gamejs.display.setMode([800, 600]);
-   gamejs.display.setCaption("Impulse");
-   // create some ship sprites and put them in a group
-
+    var canvas = document.getElementById("canvas");
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    ctx = canvas.getContext('2d');
+    canvas.addEventListener('keydown', onKeyDown);
+    canvas.addEventListener('keyup', onKeyUp);
+    canvas.addEventListener('click', onKeyUp);
+    //canvas.addEventListener("click", onClick, false);
     setupWorld(); // as you like
+    // game loop
+    // msDuration = time since last tick() call
+    step()
+});
 
+function onKeyDown(event) {
+    var keyCode = event==null? window.event.keyCode : event.keyCode;
+    switch(keyCode)
+    {
+        case 65:
+            player_x = -1;
+            break;
+        case 68:
+            player_x = 1;
+            break;
+        case 83:
+            player_y = 1;
+            break;
+        case 87:
+            player_y = -1;
+            break;
+    }
 
-   // game loop
-   var mainSurface = gamejs.display.getSurface();
-   // msDuration = time since last tick() call
-   var tick = function(msDuration) {
-         world.Step(1.0/60, 1);
-         mainSurface.fill("#FFFFFF");
-         impulse_draw.drawWorld(world, mainSurface);
-   };
-   gamejs.time.fpsCallback(tick, this, 30);
+    console.log("KEY_DOWN "+keyCode);
 }
 
-/**
- * M A I N
- */
-gamejs.ready(main);
+function onKeyUp(event) {
+    var keyCode = event==null? window.event.keyCode : event.keyCode;
+    console.log("KEY_UP "+keyCode);
+    switch(keyCode)
+    {
+        case 65:
+            player_x = 0;
+            break;
+        case 68:
+            player_x = 0;
+            break;
+        case 83:
+            player_y = 0;
+            break;
+        case 87:
+            player_y = 0;
+            break;
+    }
+}
+
+function processGame() {
+    var force = Math.abs(player_x)+Math.abs(player_y)==2 ? player_force/Math.sqrt(2) : player_force;
+    player.ApplyImpulse(new b2Vec2(force*player_x, force*player_y), player.GetCenterPosition())
+}
+
+function getCursorPosition(){
+
+    var x;
+    var y;
+    if (e.pageX || e.pageY) { 
+      x = e.pageX;
+      y = e.pageY;
+    }
+    else { 
+      x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+      y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+    } 
+    x -= gCanvasElement.offsetLeft;
+    y -= gCanvasElement.offsetTop;
+
+}
