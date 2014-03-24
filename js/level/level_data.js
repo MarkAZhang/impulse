0cfab5f6f3e45b08e0fb2fee7ef64b40420bb7cf
Mark Zhang <mazhang@mit.edu>
2014-03-24 03:36:56 -0400

    Added slightly more fighters to 4-4.

--- js/level/level_data.js
+++ js/level/level_data.js
@@ -983,7 +983,7 @@ imp_params.impulse_level_data['HIVE 4-4'] = {
   enemies: {
     "goo":  [2, 30, 0, 30, 1, 0, 1],
     "troll": [4, 15, 0, 15, 2, 1, 5],
-    "fighter": [2, 18, 3.6, 7.2, 1, 0, 5],
+    "fighter": [2, 15, 3, 6, 1, 0, 5],
     "slingshot": [1, 12, 2.4, 4.8, 1, 0, 5],
     "orbiter": [0, 12, 0, 12, 2, 2, 10]
   },
