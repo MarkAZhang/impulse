9bce8a96475e2114ba153089808842ed7347c8eb
Mark Zhang <mazhang@kensho.com>
2015-03-26 10:43:09 -0400

    Deleting old compress scripts.

--- js/compress_all.sh
+++
@@ -1,2 +0,0 @@
-./compressjs.sh vendor/*.js game_vars.js draw.js quest_data.js util.js music_player.js hive_numbers.js core/*.js player/*.js enemy/*.js obstacle/*.js lib/visibility_graph.js impulse_main.js level/*.js game_states/*.js ui/*.js
-
